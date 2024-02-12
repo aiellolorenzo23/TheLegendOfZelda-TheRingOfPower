04e54d5d398ca0f9097312200e243ea67af3d6d3
aiellolorenzo23 <aiellolorenzo23@gmail.com>
2024-02-12 14:59:52 +0100

    first commit

---
+++ src/main/java/legend/zelda/The/Legend/of/Zelda/The/Ring/of/Power/Live/TheLegendOfZeldaTheRingOfPower.java
@@ -0,0 +1,13 @@
+package legend.zelda.The.Legend.of.Zelda.The.Ring.of.Power.Live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLegendOfZeldaTheRingOfPower {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLegendOfZeldaTheRingOfPower.class, args);
+	}
+
+}
---
+++ src/test/java/legend/zelda/The/Legend/of/Zelda/The/Ring/of/Power/Live/TheLegendOfZeldaTheRingOfPowerLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package legend.zelda.The.Legend.of.Zelda.The.Ring.of.Power.Live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheLegendOfZeldaTheRingOfPowerLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
